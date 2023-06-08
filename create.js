23522103ca0b30af1580200bdfdf62950a7fbe72
Joseph Carter <josephc@Josephs-Air.home>
2023-06-07 21:26:35 -0400

    working on create feature, still buggy broght it out of src file to test and lof more easily needs to be fixed and put back in src and helpers file.

---
+++ create.js
@@ -0,0 +1,23 @@
+const { nanoid } = require("nanoid");
+const { faker} = require("@faker-js/faker");
+const _ = require("lodash");
+
+// Create a function that generates prodects 
+function createItems() {
+    //create an object with important key
+    products = {
+        // create ID for product
+        id: nanoid(4),
+        //generate product name
+        itemName: faker.commerce.product(),
+        //tells if the item is in stock or not 
+        inStock: faker.datatype.boolean(),
+        //gives a price with dollar and cents between 2 numbers only if the item is in stock if not it will say its not
+        price: products.keys(products).find(inStock => products[inStock] === true) ? faker.finance.amount({ min: 1, max: 50, dec: 2, symbol: '$' }) : "Item is not in stock at the moment.",
+        //tells how much of that item is in stock according to inStock being true, if false it will say otherwise 
+        amountInStock: products.keys(products).find(inStock => products[inStock] === true) ? faker.number.int({min: 1, max: 15}) : "N/A"
+    }
+    //return object with valid keys 
+    console.log(products);
+}
+createItems()
