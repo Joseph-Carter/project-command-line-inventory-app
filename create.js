23a6a75137dfbbbfdf8a2dba757608368f4549b4
Joseph Carter <josephc@Josephs-Air.home>
2023-06-07 21:40:57 -0400

    create function working properly and completed.

--- create.js
+++ create.js
@@ -5,18 +5,19 @@ const _ = require("lodash");
 // Create a function that generates prodects 
 function createItems() {
     //create an object with important key
-    products = {
+    let products = {
         // create ID for product
         id: nanoid(4),
         //generate product name
         itemName: faker.commerce.product(),
         //tells if the item is in stock or not 
         inStock: faker.datatype.boolean(),
-        //gives a price with dollar and cents between 2 numbers only if the item is in stock if not it will say its not
-        price: products.keys(products).find(inStock => products[inStock] === true) ? faker.finance.amount({ min: 1, max: 50, dec: 2, symbol: '$' }) : "Item is not in stock at the moment.",
+        //gives a price with dollar and cents between 2 numbers 
+        price: faker.finance.amount({ min: 1, max: 50, dec: 2, symbol: '$' }),
         //tells how much of that item is in stock according to inStock being true, if false it will say otherwise 
-        amountInStock: products.keys(products).find(inStock => products[inStock] === true) ? faker.number.int({min: 1, max: 15}) : "N/A"
+        amountInStock: null,
     }
+    products.amountInStock = Object.keys(products).find(inStock => products[inStock] === true) ? faker.number.int({min: 1, max: 15}) : "Item is not in stock at the moment.";
     //return object with valid keys 
     console.log(products);
 }
