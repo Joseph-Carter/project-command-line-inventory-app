357eaf170fd6cf1a13dcc134b18d7cbaf3cbf404
Joseph Carter <josephc@Josephs-Air.home>
2023-06-11 09:25:56 -0400

    finished updated feature, now working on pulling all files together in main file.

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ const { showItemsInCart } = require('./src/helpers/list')
 const { updateCart } = require('./src/helpers/update')
 const {createItems, itemHolder} = require("./src/create")
 
-
+const command = process.argv[2]
 
 switch (command) {
     case 'destroy': {
-      
+      //calls the deleteProduct() function to delete an item form the cart.
       break;
     }
     case 'show':
-     
+     //calls the showItemInCart() function to display the items in the cart
       break;
     case 'update': {
-     
+     // retrieves the ID, availability, stock, and price from the coommand-line arguments
+     // calls the updateCart() function to update an item in the cartw
       break;
     }
     case 'create': {
-
+      //retrieves the number of items from the command line argument
+      // generates the items using the itemHolder() function
+      //writes the items to the cartlist.json file
       break;
     }
     default:
--- src/helpers/update.js
+++ src/helpers/update.js
@@ -5,7 +5,32 @@ function updateCart(id, inStock, stock, price) {
   // Read the contents of the 'cartlist.json' file
   fs.readFile('./data/cartlist.json', 'utf8', (err, data) => {
     // Check for errors during file reading
-    
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+    // Parse the JSON data into an array of objects
+    let cartList = JSON.parse(data);
+    // Find the item in the cart with the specified ID
+    const itemsInCart = _.find(cartList, { id: id });
+    // Check if the item exists in the cart
+    if (itemsInCart) {
+        // Update the item properties with the provided values
+        itemsInCart.inStock = inStock;
+        itemsInCart.amountInStock = stock;
+        itemsInCart.price = price;
+        // Write the modified cart back to the 'cartlist.json' file
+        fs.writeFile('./data/cartlist.json', JSON.stringify(cartList, null, 2), (err) => {
+          // Check for errors during file writing
+          if (err) {
+            console.error('Error writing file:', err);
+          } else {
+            console.log(`Item with ID ${id} has been updated.`);
+          }
+        });
+      } else {
+        console.log(`No item with ID ${id} has been found.`);
+      }
     });
   }
   
